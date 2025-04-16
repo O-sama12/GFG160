f27dd6ba9ef47c3e6fd69b5361faf9150ad11b38
Osama Abdul Qader <oaqader4@gmail.com>
2025-04-16 20:05:48 +0530

    updated GFG160 day4

--- rotatearr.cpp
+++ rotatearr.cpp
@@ -3,10 +3,14 @@ using namespace std;
 int main(){
     int arr[5] = {1,2,3,4,5};
     int d = 2;
-    int n = arr.size();
+    int n = sizeof(arr) / sizeof(arr[0]);
     d %= n;
     reverse(arr.begin(), arr.begin() + d);
     reverse(arr.begin() + d, arr.end());
     reverse(arr.begin(), arr.end());
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
-
